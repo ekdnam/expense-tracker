c259558bd2e39db6bef7b4adcd0fac3ee938cffd
ekdnam <adeetya.m@gmail.com>
2021-02-09 12:20:14 +0530

    add node backend

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,9 +1,11 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
-
+import axios from 'axios';
 // Initial state
 const initialState = {
-    transactions: []
+    transactions: [],
+    error: null,
+    loading: true
 }
 
 // Create context
@@ -14,6 +16,22 @@ export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
     // Actions
+    async function getTransactions() {
+        try {
+            const res = await axios.get('/api/v1/transactions');
+
+            dispatch({
+                type: 'GET_TRANSACTIONS',
+                payload: res.data.data
+            });
+        } catch (error) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: error.response.data.error
+            });
+        }
+    }
+
     function deleteTransaction(id) {
         dispatch({
             type: 'DELETE_TRANSACTION',
@@ -30,6 +48,9 @@ export const GlobalProvider = ({ children }) => {
 
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
         deleteTransaction,
         addTransaction
     }}>
